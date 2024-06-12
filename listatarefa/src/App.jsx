2081579381388b0a51c3cb0ab75abbefbc292c6a
JOÃO VITOR LUCAS SCHMIDT <joao_vl_schmidt@estudante.sesisenai.org.br>
2024-06-12 11:21:26 -0300

    lista tarefas

---
+++ listatarefa/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+import Listatarefa from './components/Listatarefa'
+
+function App() {
+  
+
+  return (
+    <>
+      <Listatarefa/>
+    </>
+  )
+}
+
+export default App
---
+++ listatarefa/src/components/Listatarefa.jsx
@@ -0,0 +1,86 @@
+//importando os hooks
+import { useCallback, useReducer, useEffect, useState } from "react";
+
+const tarefasReducer = (state, action) => {
+    switch(action.type){
+        case 'add_tarefa':
+            return[...state, action.payload]
+            //payload é dado atual (valor da tarefa) que está sendo adicionado 
+            //na nossa lista
+        case 'concluir_tarefa': 
+            const atualizarTarefa = [...state]
+            atualizarTarefa[action.payload].completed = true
+            //utilizando a função completed para marcar como verdadeiro
+            //quando o nosso dado atual (payload) for marcado como concluido
+            return atualizarTarefa 
+        case 'excluir_tarefa':
+            return state.filter((_,index) => index !== action.payload)
+            default:
+                return state;
+    }
+}
+
+//funcao lista de tarefas
+export default function ListaTarefas(){
+    //definir as variaveis da nossa lista
+    const [tarefa, setTarefa] = useState('')
+
+    //usando o dispatch para mandar as ações pro nosso useReducer executar
+    const [tarefaAtual, dispatch] = useReducer(tarefasReducer, [])
+
+    //usando o useCallback para adicionar as tarefas na memoria cache
+    const addTarefa = useCallback(() => {
+        //verificando se a tarefa não está vazia para que seja
+        //armazenada na memoria cache
+        if(tarefa.trim() !== ''){
+            dispatch({type: 'add_tarefa', payload: {text: tarefa, completed: false}})
+        }
+    }, [tarefa])
+
+
+    //funcao para marcar tarefa como concluida
+    const concluirTarefa = useCallback((index) => {
+        dispatch({type: 'concluir_tarefa', payload: index })
+    , []})
+
+    const excluirTarefa = useCallback((index) => {
+        dispatch({type: 'excluir_tarefa', payload: index });
+    }, []);
+
+    return(
+        <div className="center">
+            <h1>Lista de Tarefas</h1>
+            <div className="inputArea">
+                <input 
+                    type="text"
+                    placeholder="Nova tarefa"
+                    value={tarefa}
+                    onChange={(e) => setTarefa(e.target.value)}
+                />
+                <button onClick={addTarefa}>Add tarefa</button>
+            </div>
+            <ul>
+                {/*Criando a lista com as tarefas que forem sendo adicionadas 
+                    vamos usar o .map para mapear cada tareda 
+                    usar o index para identificar a posição de cada tarefa adicionada
+                */}
+
+                {tarefaAtual.map((tarefas, index) =>(
+                    //lista de acordo com a posição da tarefa anterior
+                    <li key={index}>
+                    <span style={{textDecoration: tarefas.completed? 'line-through' : 'none' }}>
+                        {tarefas.text}
+                    </span>
+                    {!tarefas.completed && (
+                        <>
+                            <button onClick={() => concluirTarefa(index)}>Concluir</button>
+                            <button onClick={() => excluirTarefa(index)}>Excluir</button>
+                        </>
+                    )}
+                </li>
+                ))}
+            </ul>
+        </div>
+    )
+
+}
